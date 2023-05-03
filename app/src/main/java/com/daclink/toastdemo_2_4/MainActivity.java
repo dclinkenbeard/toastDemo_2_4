8a3f0618a3278cfc1e49484af5bdc328bfa69d09
dclinkenbeard <dclinkenbeard@csumb.edu>
2023-05-03 14:38:10 -0700

    Added a toast and a button to the layout. Shouldn't cause any issues...

--- app/src/main/java/com/daclink/toastdemo_2_4/MainActivity.java
+++ app/src/main/java/com/daclink/toastdemo_2_4/MainActivity.java
@@ -2,7 +2,12 @@ package com.daclink.toastdemo_2_4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+
+import io.github.muddz.styleabletoast.StyleableToast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View v){
+        new StyleableToast
+                .Builder(getApplicationContext())
+                .text("Hello world!")
+                .textColor(Color.WHITE)
+                .backgroundColor(Color.BLUE)
+                .gravity(Gravity.TOP)
+                .show();
     }
 }
